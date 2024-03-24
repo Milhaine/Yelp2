7ac02216fa017d9050c0ed8045426a6c5b8901ab
Mylene Leblanc <leblanc.pommier.mylene@gmail.com>
2024-03-24 10:55:42 +0100

    model + validates

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-  validates :name, :category, presence: true
+  validates :name, :address, :category, presence: true
   validates :category, inclusion: { in: %w(chinese italian japanese belgian french) }
   has_many :reviews, dependent: :destroy
 end
